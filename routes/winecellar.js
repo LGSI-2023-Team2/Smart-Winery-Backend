2831144555951c9857851b93ee5104e688f5723b
re1yun <jaeyunlim@gmail.com>
2023-07-17 08:28:16 +0530

    23.07.17

--- routes/winecellar.js
+++ routes/winecellar.js
@@ -189,10 +189,29 @@ module.exports = function(app){
                     // step 2. update cellar setting
                     console.log("[WINECELLAR]:: winecellar found");
                     console.log(cellar);
+                    cellar.floor1.type = setting.floor1.type;
+                    cellar.floor1.temperature_now = setting.floor1.temperature_now;
+                    cellar.floor1.temperature_target = setting.floor1.temperature_target;
+                    cellar.floor1.is_smart_mode = setting.floor1.is_smart_mode;
+                    cellar.floor2.type = setting.floor2.type;
+                    cellar.floor2.temperature_now = setting.floor2.temperature_now;
+                    cellar.floor2.temperature_target = setting.floor2.temperature_target;
+                    cellar.floor2.is_smart_mode = setting.floor2.is_smart_mode;
+                    cellar.floor3.type = setting.floor3.type;
+                    cellar.floor3.temperature_now = setting.floor3.temperature_now;
+                    cellar.floor3.temperature_target = setting.floor3.temperature_target;
+                    cellar.floor3.is_smart_mode = setting.floor3.is_smart_mode;
+                    cellar.save();
+                    res.json(cellar);
+
+
+                    /* 
                     cellar.floor1.cell_ids.forEach(function(curCell){
                         if(curCell.col == setting.col){
-                            curCell.temp = setting.temp;
-                            curCell.humi = setting.humi;
+                            curCell.type = setting.type;
+                            curCell.temperature_now = setting.temperature_now;
+                            curCell.temperature_target = setting.temperature_target;
+                            curCell.is_smart_mode = setting.is_smart_mode;
                             curCell.save();
                         }
                     });
@@ -209,9 +228,8 @@ module.exports = function(app){
                             curCell.humi = setting.humi;
                             curCell.save();
                         }
-                    });
+                    }); */
                     // step 3. return cellar setting info
-                    res.json(cellar);
                 })
                 .catch(function(err){
                     console.log("[WINECELLAR]:: winecellar data ERROR!");
@@ -241,27 +259,19 @@ module.exports = function(app){
                     // step 2. update cellar setting
                     console.log("[WINECELLAR]:: winecellar found");
                     console.log(cellar);
-                    cellar.floor1.cell_ids.forEach(function(curCell){
-                        if(curCell.col == setting.col){
-                            curCell.temp = setting.temp;
-                            curCell.humi = setting.humi;
-                            curCell.save();
-                        }
-                    });
-                    cellar.floor2.cell_ids.forEach(function(curCell){
-                        if(curCell.col == setting.col){
-                            curCell.temp = setting.temp;
-                            curCell.humi = setting.humi;
-                            curCell.save();
-                        }
-                    });
-                    cellar.floor3.cell_ids.forEach(function(curCell){
-                        if(curCell.col == setting.col){
-                            curCell.temp = setting.temp;
-                            curCell.humi = setting.humi;
-                            curCell.save();
-                        }
-                    });
+                    cellar.floor1.type = setting.floor1.type;
+                    cellar.floor1.temperature_now = setting.floor1.temperature_now;
+                    cellar.floor1.temperature_target = setting.floor1.temperature_target;
+                    cellar.floor1.is_smart_mode = setting.floor1.is_smart_mode;
+                    cellar.floor2.type = setting.floor2.type;
+                    cellar.floor2.temperature_now = setting.floor2.temperature_now;
+                    cellar.floor2.temperature_target = setting.floor2.temperature_target;
+                    cellar.floor2.is_smart_mode = setting.floor2.is_smart_mode;
+                    cellar.floor3.type = setting.floor3.type;
+                    cellar.floor3.temperature_now = setting.floor3.temperature_now;
+                    cellar.floor3.temperature_target = setting.floor3.temperature_target;
+                    cellar.floor3.is_smart_mode = setting.floor3.is_smart_mode;
+                    cellar.save();
                     // step 3. return cellar setting info
                     res.json(cellar);
                 })
