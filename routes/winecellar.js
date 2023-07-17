4e31eae751378b8dbe11347c42d84160a673a59d
re1yun <jaeyunlim@gmail.com>
2023-07-17 14:23:59 +0530

    23.07.17

--- routes/winecellar.js
+++ routes/winecellar.js
@@ -59,45 +59,43 @@ module.exports = function(app){
                 })
                 .then(function(cellar){
                     // step 2. find wine
-                    var floor = req.body.row;
-                    var cellID;
-                    console.log(cellar);
-                    if(floor == 1){
-                        cellar.floor1.cell_ids.forEach(function(curCell){
-                            console.log(curCell);
-                            if(curCell.col == req.body.col){
-                                cellID = curCell._id;
-                                console.log("found!");
+                    // step 2-1. find cell
+                    var cellId;
+                    if(req.body.row == 1){
+                        cellar.floor1.cell_ids.forEach(function(cell){
+                            if(cell.row == req.body.row && cell.col == req.body.col){
+                                cellId = cell._id;
+                                cellar.floor1.cell_ids.pull(cellId);
                             }
                         });
                     }
-                    else if(floor == 2){
-                        cellar.floor2.cell_ids.forEach(function(curCell){
-                            if(curCell.col == req.body.col){
-                                cellID = curCell._id;
-                                console.log("found!");
+                    else if(req.body.row == 2){
+                        cellar.floor2.cell_ids.forEach(function(cell){
+                            if(cell.row == req.body.row && cell.col == req.body.col){
+                                cellId = cell._id;
+                                cellar.floor2.cell_ids.pull(cellId);
                             }
                         });
                     }
-                    else if(floor == 3){
-                        cellar.floor3.cell_ids.forEach(function(curCell){
-                            if(curCell.col == req.body.col){
-                                cellID = curCell._id;
-                                console.log("found!");
+                    else if(req.body.row == 3){
+                        cellar.floor3.cell_ids.forEach(function(cell){
+                            if(cell.row == req.body.row && cell.col == req.body.col){
+                                cellId = cell._id;
+                                cellar.floor3.cell_ids.pull(cellId);
                             }
                         });
                     }
-                    // step 3. delete wine
-                    Cell.deleteOne({_id: cellID})
-                        .then(function(msg){
-                            console.log("[WINECELLAR]:: delete Success cellId: ", cellID);
+                    cellar.save();
+                    // step 2-2. remove wine from cell
+                    Cell.deleteOne({_id: cellId})
+                        .then(function(){
+                            console.log("[WINECELLAR]:: wine removed from cell");
                             res.json(cellar);
                         })
                         .catch(function(err){
+                            console.log(err);
                             res.json(err);
-                            console.log("[WINECELLAR]:: delete failed");
                         })
-                    
                 })
                 .catch(function(err){
                     res.json(err);
@@ -156,18 +154,6 @@ module.exports = function(app){
             console.log("[WINECELLAR]:: winecellar setting request received");
             // step 1. find cellar
             Cellar.findOne({_id: req.query.id})
-                .populate({
-                    path: 'floor1.cell_ids',
-                    model: Cell
-                })
-                .populate({
-                    path: 'floor2.cell_ids',
-                    model: Cell
-                })
-                .populate({
-                    path: 'floor3.cell_ids',
-                    model: Cell
-                })
                 .then(function(cellar){
                     // step 2. return cellar setting info
                     console.log("[WINECELLAR]:: winecellar found");
@@ -186,106 +172,24 @@ module.exports = function(app){
             var setting = req.body;
             // step 1. find cellar
             Cellar.findOne({_id: req.body.cellarid})
-                .populate({
-                    path: 'floor1.cell_ids',
-                    model: Cell
-                })
-                .populate({
-                    path: 'floor2.cell_ids',
-                    model: Cell
-                })
-                .populate({
-                    path: 'floor3.cell_ids',
-                    model: Cell
-                })
                 .then(function(cellar){
                     // step 2. update cellar setting
                     console.log("[WINECELLAR]:: winecellar found");
                     console.log(cellar);
-                    cellar.floor1.type = setting.floor1.type;
-                    cellar.floor1.temperature_now = setting.floor1.temperature_now;
-                    cellar.floor1.temperature_target = setting.floor1.temperature_target;
-                    cellar.floor1.is_smart_mode = setting.floor1.is_smart_mode;
-                    cellar.floor2.type = setting.floor2.type;
-                    cellar.floor2.temperature_now = setting.floor2.temperature_now;
-                    cellar.floor2.temperature_target = setting.floor2.temperature_target;
-                    cellar.floor2.is_smart_mode = setting.floor2.is_smart_mode;
-                    cellar.floor3.type = setting.floor3.type;
-                    cellar.floor3.temperature_now = setting.floor3.temperature_now;
-                    cellar.floor3.temperature_target = setting.floor3.temperature_target;
-                    cellar.floor3.is_smart_mode = setting.floor3.is_smart_mode;
-                    cellar.save();
-                    res.json(cellar);
-
-
-                    /* 
-                    cellar.floor1.cell_ids.forEach(function(curCell){
-                        if(curCell.col == setting.col){
-                            curCell.type = setting.type;
-                            curCell.temperature_now = setting.temperature_now;
-                            curCell.temperature_target = setting.temperature_target;
-                            curCell.is_smart_mode = setting.is_smart_mode;
-                            curCell.save();
-                        }
-                    });
-                    cellar.floor2.cell_ids.forEach(function(curCell){
-                        if(curCell.col == setting.col){
-                            curCell.temp = setting.temp;
-                            curCell.humi = setting.humi;
-                            curCell.save();
-                        }
-                    });
-                    cellar.floor3.cell_ids.forEach(function(curCell){
-                        if(curCell.col == setting.col){
-                            curCell.temp = setting.temp;
-                            curCell.humi = setting.humi;
-                            curCell.save();
-                        }
-                    }); */
+                    cellar.floor1.type = setting.floor1_type;
+                    cellar.floor1.temperature_now = setting.floor1_temperature_now;
+                    cellar.floor1.temperature_target = setting.floor1_temperature_target;
+                    cellar.floor1.is_smart_mode = setting.floor1_is_smart_mode;
+                    cellar.floor2.type = setting.floor2_type;
+                    cellar.floor2.temperature_now = setting.floor2_temperature_now;
+                    cellar.floor2.temperature_target = setting.floor2_temperature_target;
+                    cellar.floor2.is_smart_mode = setting.floor2_is_smart_mode;
+                    cellar.floor3.type = setting.floor3_type;
+                    cellar.floor3.temperature_now = setting.floor3_temperature_now;
+                    cellar.floor3.temperature_target = setting.floor3_temperature_target;
+                    cellar.floor3.is_smart_mode = setting.floor3_is_smart_mode;
                     // step 3. return cellar setting info
-                })
-                .catch(function(err){
-                    console.log("[WINECELLAR]:: winecellar data ERROR!");
-                    res.json(err);
-                });
-        })
-        // !!! PUT !!!
-        // !!! function to set cellar setting !!!
-        .put(function(req, res){
-            console.log("[WINECELLAR]:: winecellar setting request received");
-            var setting = req.body;
-            // step 1. find cellar
-            Cellar.findOne({_id: req.body.cellarid})
-                .populate({
-                    path: 'floor1.cell_ids',
-                    model: Cell
-                })
-                .populate({
-                    path: 'floor2.cell_ids',
-                    model: Cell
-                })
-                .populate({
-                    path: 'floor3.cell_ids',
-                    model: Cell
-                })
-                .then(function(cellar){
-                    // step 2. update cellar setting
-                    console.log("[WINECELLAR]:: winecellar found");
-                    console.log(cellar);
-                    cellar.floor1.type = setting.floor1.type;
-                    cellar.floor1.temperature_now = setting.floor1.temperature_now;
-                    cellar.floor1.temperature_target = setting.floor1.temperature_target;
-                    cellar.floor1.is_smart_mode = setting.floor1.is_smart_mode;
-                    cellar.floor2.type = setting.floor2.type;
-                    cellar.floor2.temperature_now = setting.floor2.temperature_now;
-                    cellar.floor2.temperature_target = setting.floor2.temperature_target;
-                    cellar.floor2.is_smart_mode = setting.floor2.is_smart_mode;
-                    cellar.floor3.type = setting.floor3.type;
-                    cellar.floor3.temperature_now = setting.floor3.temperature_now;
-                    cellar.floor3.temperature_target = setting.floor3.temperature_target;
-                    cellar.floor3.is_smart_mode = setting.floor3.is_smart_mode;
                     cellar.save();
-                    // step 3. return cellar setting info
                     res.json(cellar);
                 })
                 .catch(function(err){
